b7bd38246a9142786305704e8b3e2edd7d424707
NITHIN-20053 <rnithin2105@gmail.com>
2025-11-18 18:59:02 +0000

    UI Script
    Added a StartMenuManager and added a basic script to control how the UI flow works

---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class StartMenu : MonoBehaviour
+{
+    public GameObject PlayerViewScreen;
+    public GameObject MainScreen;
+    public GameObject PauseScreen;
+
+    public void ChangeScreenToPlayerView()
+    {
+        Debug.Log("Start Button pressed");
+        PlayerViewScreen.SetActive(true);
+    }
+    public void ChangeScreenToMainScreen()
+    {
+        Debug.Log("Back Button pressed");
+        PlayerViewScreen.SetActive(false);
+    }
+    public void ChangeScreenToGameScreen()
+    {
+        PlayerViewScreen.SetActive(false);
+        MainScreen.SetActive(false);
+    }
+    public void ChangeScreenToGamePlayScreen()
+    {
+        PlayerViewScreen.SetActive(false);
+        MainScreen.SetActive(false);
+        PauseScreen.SetActive(false);
+    }
+
+}
