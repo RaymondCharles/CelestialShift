d1c4268608e638a33c35f580d921f86a413ed65c
NITHIN-20053 <rnithin2105@gmail.com>
2025-12-26 14:38:48 +0000

    Added Menu To Main
    Created a new scene Menu added the required UI and functionality when a button is pressed

---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+    public GameObject PlayerViewScreen;
+    public GameObject MainScreen;
+
+    // Change to Player View Screen When Start is Pressed
+
+    public void ChangeScreenToPlayerView()
+    {
+        PlayerViewScreen.SetActive(true);
+    }
+    // Change to Main View Screen When Back is Pressed
+
+    public void ChangeScreenToMainScreen()
+    {
+        PlayerViewScreen.SetActive(false);
+    }
+
+    // Change to Game Scene When Play is Pressed
+    public void OnClickPlay()
+    {
+        LoadingManager.Instance.ChangeToGameScene(1);
+    }
+ 
+
+
+
+}
