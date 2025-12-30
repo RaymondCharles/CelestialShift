7cc73bb6f8c370d3999ff22ef4095cdbfb51f63c
NITHIN-20053 <rnithin2105@gmail.com>
2025-12-30 15:06:52 +0000

    Implementation Save  v1
    Did save button for Player positon

--- Assets/Scripts/PlayerMotion.cs
+++ Assets/Scripts/PlayerMotion.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
 public class PlayerMotion : MonoBehaviour
 {
@@ -17,6 +19,40 @@ public class PlayerMotion : MonoBehaviour
     public GameObject PausePanel;
     public Transform playerTransform;
     public float speed = 5.0f;
+    public int level = 0; // save feature testing
+
+    public void SavePlayer()
+    {
+        SaveSystem.SavePlayer(this);
+        SceneManager.LoadScene("MenuScene");
+
+    }
+    //public void LoadPlayer()
+    //{
+    //    PlayerData data = SaveSystem.LoadPlayer();
+    //    level= data.level;
+    //    Vector3 position;
+    //    position.x = data.position[0];
+    //    position.y = data.position[1];
+    //    position.z = data.position[2];
+    //    transform.position = position;
+    //}
+    public void LoadPlayer()
+    {
+        PlayerData data = SaveSystem.LoadPlayer();
+        if (data == null)
+        {
+            Debug.LogError("No saved player data found!");
+            return;
+        }
+
+        level = data.level;
+        Vector3 position = new Vector3(data.position[0], data.position[1], data.position[2]);
+        Debug.Log("Loading position: " + position);
+        transform.position = position;
+    }
+
+
 
 
     private void Awake()
@@ -39,6 +75,22 @@ public class PlayerMotion : MonoBehaviour
         InverntoryAction = playerInput.actions.FindAction("Inventory");
         PauseAction = playerInput.actions.FindAction("Pause");
         DropAction = playerInput.actions.FindAction("Drop");
+
+        if (GameManager.Instance != null)
+        {
+            Debug.Log("GameManager instance found. loadGame = " + GameManager.Instance.loadGame);
+
+            if (GameManager.Instance.loadGame)
+            {
+                Debug.Log("Loading player...");
+                LoadPlayer();
+                GameManager.Instance.loadGame = false;
+            }
+        }
+        else
+        {
+            Debug.LogError("No GameManager instance found!");
+        }
 
     }
 
@@ -77,10 +129,21 @@ public class PlayerMotion : MonoBehaviour
         Vector2 direction = moveAction.ReadValue<Vector2>();
         transform.position += new Vector3(direction.x, 0, direction.y) * Time.deltaTime * speed;
     }
-    public void OnClickPlay()
+    public void OnClick()
     {
-        LoadingManager.Instance.ChangeToGameScene(0);
+        if (GameManager.Instance != null)
+        {
+            GameManager.Instance.SaveAndQuit();
+        }
+        else
+        {
+            Debug.LogError("GameManager instance not found!");
+        }
     }
+    //public void OnClickPlay()
+    //{
+    //    LoadingManager.Instance.ChangeToGameScene(0);
+    //}
     //public void DropSelectedItem()
     //{
     //    if (Inventory.Instance.items.Count > 0)
