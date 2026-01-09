0a9aa6cb202f81a47fdd3c6e44dcf0549685054c
Raymond Mario Charles <raymondmariocharles@gmail.com>
2026-01-09 17:48:54 +0000

    Added Fire effect when fire potion thrown at Enemy

--- Assets/Scripts/Enemy Scrpts/EnemyBurn.cs
+++ Assets/Scripts/Enemy Scrpts/EnemyBurn.cs
@@ -11,6 +11,12 @@ public class EnemyBurn : MonoBehaviour
     private float dps;             // damage per second
     private float tickInterval = 1f;
 
+    // VFX
+    [SerializeField] private GameObject fireVfxPrefab;     // assign prefab here
+    [SerializeField] private Vector3 vfxLocalOffset = new Vector3(0f, 1f, 0f);
+    private GameObject fireVfxInstance;
+
+
     private void Awake()
     {
         if (enemyHealth == null)
@@ -20,37 +26,15 @@ public class EnemyBurn : MonoBehaviour
     // Call this when hit by fire potion
     public void ApplyBurn(float damagePerSecond, float durationSeconds, float tickEverySeconds = 1f)
     {
+        
         dps = Mathf.Max(dps, damagePerSecond); 
         tickInterval = Mathf.Max(0.05f, tickEverySeconds);
         burnUntil = Mathf.Max(burnUntil, Time.time + durationSeconds);
 
         if (burnRoutine == null)
+            EnsureFireVfx();
             burnRoutine = StartCoroutine(BurnLoop());
     }
-
-    //private IEnumerator BurnLoop()
-    //{
-    //    // next tick after 1 second
-    //    float nextTick = Time.time + tickInterval;
-
-    //    while (Time.time <= burnUntil + 0.0001f)
-    //    {
-    //        if (Time.time >= nextTick)
-    //        {
-    //            if (enemyHealth != null)
-    //            {
-    //                enemyHealth.TakeDamage(dps); 
-    //            }
-
-    //            nextTick += tickInterval;
-    //        }
-
-    //        yield return null;
-    //    }
-
-    //    burnRoutine = null;
-    //    dps = 0f;
-    //}
 
     private IEnumerator BurnLoop()
     {
@@ -64,8 +48,29 @@ public class EnemyBurn : MonoBehaviour
             yield return new WaitForSeconds(tickInterval);
         }
 
+        StopFireVfx();
+
         burnRoutine = null;
         dps = 0f;
+
+    }
+
+    private void EnsureFireVfx()
+    {
+        if (fireVfxPrefab == null) return;
+        if (fireVfxInstance != null) return;
+
+        fireVfxInstance = Instantiate(fireVfxPrefab, transform);
+        fireVfxInstance.transform.localPosition = vfxLocalOffset;
+        fireVfxInstance.transform.localRotation = Quaternion.identity;
+    }
+
+    private void StopFireVfx()
+    {
+        if (fireVfxInstance == null) return;
+        Destroy(fireVfxInstance);
+        fireVfxInstance = null;
     }
 
 }
+
